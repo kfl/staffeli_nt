740da121ba398276396ceb363c5e5dc662441a71
Ken Friis Larsen <ken@friislarsen.net>
2020-09-15 19:44:12 +0200

    Upload grade for a single submission

---
+++ staffeli_nt/upload_single.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import tempfile
+
+from os import access, R_OK
+from os.path import isfile
+
+
+from pathlib import Path
+
+from vas import *
+from util import *
+
+NAME_SHEET = 'grade.yml'
+
+def grade(submission, grade, path_feedback, dry_run=True):
+    # bail if dry
+    print('Submit: user_id=%d, grade=%s' % (submission.user_id, grade))
+    if dry_run:
+        return
+
+    # set grade and load comments
+    submission.edit(submission={'posted_grade': grade})
+
+    # check if feedback is already uploaded
+    # duplicate = False
+    # for comment in submission.submission_comments:
+    #     try:
+    #         attachments = comment['attachments']
+    #     except KeyError:
+    #         attachments = []
+
+    #     print('Comment with:', len(attachments), 'attachments')
+    #     for attachment in attachments:
+    #         if duplicate:
+    #             break
+
+    #         try:
+    #             contents = download(attachment['url']).decode('utf-8')
+    #         except UnicodeDecodeError:
+    #             contents = ''
+
+    #         duplicate = duplicate or contents.strip() == feedback.strip()
+
+    # # upload feedback if new
+    # if duplicate:
+    #     print('Feedback already uploaded:', submission.user_id)
+
+    # else:
+    print('Uploading new feedback:', submission.user_id)
+    submission.upload_comment(path_feedback)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print("Usage: upload_single <POINTS> <meta.yml> <grade.yml> <feedback.txt> [--live]")
+        return
+
+    points = sys.argv[1]
+    path_meta_yml = sys.argv[2]
+    path_grade_yml = sys.argv[3]
+    path_feedback = sys.argv[4]
+
+    live = '--live' in sys.argv
+
+    path_token = os.path.join(
+        str(Path.home()),
+        '.canvas.token'
+    )
+
+    with open(path_meta_yml, 'r') as f:
+        meta = parse_meta(f.read())
+
+    with open(path_token, 'r') as f:
+        API_KEY = f.read().strip()
+
+    # get grade.yml
+    with open(path_grade_yml, 'r') as f:
+        sheet =  parse_sheet(f.read())
+
+    if not(isfile(path_feedback) and access(path_feedback, R_OK)):
+        print(f"File {path_feedback} doesn't exist or isn't readable")
+        return
+
+    # obtain canvas session
+    API_URL = 'https://absalon.ku.dk/'
+
+    canvas = Canvas(API_URL, API_KEY)
+    course = canvas.get_course(meta.course.id)
+    assignment = course.get_assignment(meta.assignment.id)
+
+    if live:
+        print('Uploading feedback to:', assignment)
+        choice = input('Sure? (y/n) : ')
+        assert choice.strip() == 'y'
+    else:
+        print('Doing a dry-run...')
+
+    for student in sheet.students:
+        submission = assignment.get_submission(student.id)
+        total = points
+
+        grade(
+            submission,
+            total,
+            path_feedback,
+            dry_run = not live
+        )
