7d31e94225077b7cf5ab8d9a3da493ce1a2534f7
Troels Henriksen <athas@sigkill.dk>
2025-09-24 16:52:48 +0200

    Update shell.nix (#68)
    Fixes #66.

--- shell.nix
+++ shell.nix
@@ -1,20 +1,22 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     nativeBuildInputs = [
-      (pkgs.python39.withPackages(ps: with ps; [
-        ruamel-yaml 
+      (pkgs.python313.withPackages(ps: with ps; [
+        ruamel-yaml
         (
           buildPythonPackage rec {
             pname = "canvasapi";
             version = "2.2.0";
             src = fetchPypi {
               inherit pname version;
-              sha256 = "5087db773cac9d92f4f4609b3c160dbeeceb636801421808afee2d438bc43f62";
+              sha256 = "sha256-UIfbdzysnZL09GCbPBYNvuzrY2gBQhgIr+4tQ4vEP2I=";
             };
             doCheck = false;
+            pyproject = true;
+            build-system = [ setuptools ];
             propagatedBuildInputs = [
-              pkgs.python39Packages.pytz
-              pkgs.python39Packages.requests
+              ps.pytz
+              ps.requests
             ];
           }
         )
