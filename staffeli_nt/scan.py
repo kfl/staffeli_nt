bd9180b2e28c4705c276bb013289cbfdf6dfa329
Ken Friis Larsen <ken@friislarsen.net>
2025-11-26 21:50:02 +0100

    Migrate scan.py to rich output

--- staffeli_nt/scan.py
+++ staffeli_nt/scan.py
@@ -1,16 +1,10 @@
 import argparse
 import os
 
+from .console import console, print_success, print_warning
 from .vas import *
 
 NAME_SHEET = 'grade.yml'
-
-RED = '\033[91m'
-GREEN = '\033[92m'
-YELLOW = '\033[93m'
-LIGHT_PURPLE = '\033[94m'
-PURPLE = '\033[95m'
-END = '\033[0m'
 
 
 def add_subparser(subparsers: argparse._SubParsersAction):
@@ -52,16 +46,16 @@ def main(api_url, api_key, args: argparse.Namespace):
     done = 0
     for path, sheet in sheets:
         if not sheet.is_graded(tmpl):
-            print(f'{RED}█{END} {path} is not graded')
+            console.print(f'[error]■[/error] {path} is not graded')
             graded = False
             missing += 1
         else:
             total = sheet.get_grade(tmpl)
             tp = tmpl.total_points
-            print(f'{GREEN}█{END} {total}/{tp} points for {path}')
+            console.print(f'[success]■[/success] {total}/{tp} points for {path}')
             done += 1
 
     if graded is False:
-        print(f'Grading is not complete, {done} done, {missing} missing')
+        print_warning(f'Grading is not complete, {done} done, {missing} missing')
     else:
-        print('Yay, time to upload')
+        print_success('Yay, time to upload')
